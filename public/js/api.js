0b075d661c792448a7b5d354e12a865255dda09e
jsrbrt <robmail.fds@gmail.com>
2022-12-22 18:47:45 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "3bbe270b76e7f1d734a06de4e29ec7c0"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
